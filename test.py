a79743cf8c53d9c5733572d11257752742b9d8eb
COLD MAFIA 007 <65175372+phantomzero1001@users.noreply.github.com>
2022-09-25 21:18:24 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 19:30:38 2022
+
+@author: bhsh1004
+"""
+
+import streamlit as st
+import pandas as pd
+
+
+st.title ( " Hello streamlit " )
+
+st.header("Deployment Test")
+
+st.map()
